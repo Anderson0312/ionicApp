032612d513d94e7a665127f8675e58ff9d3c1893
Anderson0312 <andersonmoura812@gmail.com>
2023-01-15 14:23:01 -0300

    create page order

--- hotelMenagment/src/app/app-routing.module.ts
+++ hotelMenagment/src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'order',
+    loadChildren: () => import('./order/order.module').then( m => m.OrderPageModule)
+  },
 ];
 
 @NgModule({
