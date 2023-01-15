8d8c19b515bc7f1522a948c4cad41b5f20054fe7
Anderson0312 <andersonmoura812@gmail.com>
2023-01-15 17:28:59 -0300

    atualization page order
    cards of prods used informations of
    database

--- hotelMenagment/src/app/order/order.page.ts
+++ hotelMenagment/src/app/order/order.page.ts
@@ -7,6 +7,105 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderPage implements OnInit {
 
+  orders= [{
+    "date": "today, 23 Aug 2021",
+    "order": [{
+      "id": "01",
+      "image": "001.jpg",
+      "title": "Hamburger",
+      "amount": "55.40",
+      "transID": "323244",
+      "time": "20 Min",
+      "status": "CONFIRM"
+    },
+    {
+      "id": "02",
+      "image": "002.jpg",
+      "title": "Hamburger",
+      "amount": "55.40",
+      "transID": "323244",
+      "time": "20 Min",
+      "status": "CONFIRM"
+    },
+    {
+      "id": "03",
+      "image": "003.jpg",
+      "title": "Hamburger",
+      "amount": "55.40",
+      "transID": "323244",
+      "time": "20 Min",
+      "status": "CANCELLED"
+    },
+    {
+      "id": "04",
+      "image": "004.jpg",
+      "title": "Hamburger",
+      "amount": "55.40",
+      "transID": "323244",
+      "time": "20 Min",
+      "status": "CONFIRM"
+    },
+    {
+      "id": "05",
+      "image": "005.jpg",
+      "title": "Hamburger",
+      "amount": "55.40",
+      "transID": "323244",
+      "time": "20 Min",
+      "status": "CONFIRM"
+    },
+    {
+      "id": "06",
+      "image": "006.jpg",
+      "title": "Hamburger",
+      "amount": "55.40",
+      "transID": "323244",
+      "time": "20 Min",
+      "status": "CANCELLED"
+    },
+    {
+      "id": "07",
+      "image": "007.jpg",
+      "title": "Hamburger",
+      "amount": "55.40",
+      "transID": "323244",
+      "time": "20 Min",
+      "status": "CONFIRM"
+    }],
+  },
+  {
+  "date": "today, 01 Febre 2022",
+  "order": [{
+    "id": "01",
+    "image": "001.jpg",
+    "title": "Hamburger",
+    "amount": "55.40",
+    "transID": "323244",
+    "time": "20 Min",
+    "status": "CONFIRM"
+  },
+  {
+    "id": "02",
+    "image": "002.jpg",
+    "title": "Hamburger",
+    "amount": "55.40",
+    "transID": "323244",
+    "time": "20 Min",
+    "status": "CANCELLED"
+  },
+  {
+    "id": "03",
+    "image": "003.jpg",
+    "title": "Hamburger",
+    "amount": "55.40",
+    "transID": "323244",
+    "time": "20 Min",
+    "status": "CONFIRM"
+  },
+]
+}
+]
+
   constructor() { }
 
   ngOnInit() {
